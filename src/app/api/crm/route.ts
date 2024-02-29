aaa7bb41b030e64e321412063163cb918da3fe76
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2024-02-29 12:34:26 -0500

    fix: env handling

--- src/app/api/crm/route.ts
+++ src/app/api/crm/route.ts
@@ -1,12 +1,14 @@
 export async function POST(request: Request) {
   const body = await request.json();
 
+  const apiKey = process.env.CRM_API_KEY;
+
   const res = await fetch(
     'https://rebelmosquito-679106251063943741.myfreshworks.com/crm/sales/api/contacts/',
     {
       method: 'POST',
       headers: {
-        Authorization: `Token token=${process.env.CRM_API_KEY}`,
+        Authorization: `Token token=${apiKey}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
